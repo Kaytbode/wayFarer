662dfc5627ee36d0387d55a36e2609fad0bf4f09
Kayode <bkaoluborode@gmail.com>
2019-07-05 18:45:17 +0100

    feat: Get all trips
    api endpoint successfully retrieves data from database.

--- db/dbController.js
+++ db/dbController.js
@@ -1,1 +1,27 @@
+import { Pool } from 'pg';
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const api = {
+  async getTrips(req, res) {
+    const getAllTrips = 'SELECT * FROM trips';
+
+    try {
+      const { rows } = await pool.query(getAllTrips);
+
+      return res.status(200).send({
+        status: 'success',
+        data: rows,
+      });
+    } catch (err) {
+      return res.status(400).send({
+        status: 'error',
+        error: err,
+      });
+    }
+  },
+};
+
+export default api;
