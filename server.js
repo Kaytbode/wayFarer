350a2f6be00fa51267841ef2758f9949d4762334
Kayode <bkaoluborode@gmail.com>
2019-07-09 17:43:38 +0100

    feat: heroku host

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(
   }),
 );
 
+app.get('/', (req, res) => res.send({ hey: 'you' }));
 app.post('/api/v1/auth/signup', api.createUser);
 app.post('/api/v1/auth/signin', api.confirmUser);
 app.post('/api/v1/trips', api.createTrip);
