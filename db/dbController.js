1c98d67e320ad1161b185608bf1b7b44020da33b
Kayode <bkaoluborode@gmail.com>
2019-07-15 18:26:26 +0100

    fix: req parameters modified

--- db/dbController.js
+++ db/dbController.js
@@ -315,7 +315,7 @@ class api {
 
   static async deleteBooking(req, res) {
     const { token } = req.body;
-
+    console.log(req.body);
     const bookingId = req.params.booking_id;
 
     if (!token) {
