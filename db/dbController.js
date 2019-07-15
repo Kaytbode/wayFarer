89b352f79d9dffbc1cc15e82bee554fa5623b23f
Kayode <bkaoluborode@gmail.com>
2019-07-15 18:53:08 +0100

    fix: req parameters modified

--- db/dbController.js
+++ db/dbController.js
@@ -240,7 +240,7 @@ class api {
       const newBooking = {
         text: `INSERT INTO booking (user_id, trip_id, created_on, bus_id, trip_date, seat_number, 
           first_name, last_name, email) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id`,
-        values: [tripId, userId, createdOn, busId, tripDate,
+        values: [userId, tripId, createdOn, busId, tripDate,
           seatNumber, firstName, lastName, email],
       };
 
