bd3b4e289a58348adf17996be488bde2e5731ef6
kaytbode <bkaoluborode@gmail.com>
2019-07-16 11:44:51 +0100

    test: trying elephantsql

--- db/index.js
+++ db/index.js
@@ -1,11 +1,17 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
+/* const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PW,
   port: process.env.DB_PORT,
+}); */
+
+const connectionString = 'postgres://igekybyt:bq3Fh6DILuXf7TVJqSSXIeGXGSBzncdo@otto.db.elephantsql.com:5432/igekybyt';
+
+const pool = new Pool({
+  connectionString,
 });
 
 export default pool;
