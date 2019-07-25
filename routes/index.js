8f9a9ceda0005200f926ab7f79243e5fb9437433
kaytbode <bkaoluborode@gmail.com>
2019-07-25 15:27:25 +0100

    feat: Version api route

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,14 @@ const router = express.Router();
 router.get('/', (req, res) => res.status(200).send({
   apiDocs: 'https://wayfarer4.docs.apiary.io/',
 }));
-router.post('/auth/signup', api.createUser);
-router.post('/auth/signin', api.confirmUser);
-router.post('/trips', api.createTrip);
-router.get('/trips', api.getTrips);
-router.post('/bookings', api.bookASeat);
-router.get('/bookings', api.viewBookings);
-router.delete('/bookings/:booking_id', api.deleteBooking);
-router.patch('/trips/:trip_id', api.cancelTrip);
+router.post('api/v1/auth/signup', api.createUser);
+router.post('api/v1/auth/signin', api.confirmUser);
+router.post('api/v1/trips', api.createTrip);
+router.get('api/v1/trips', api.getTrips);
+router.post('api/v1/bookings', api.bookASeat);
+router.get('api/v1/bookings', api.viewBookings);
+router.delete('api/v1/bookings/:booking_id', api.deleteBooking);
+router.patch('api/v1/trips/:trip_id', api.cancelTrip);
 
 // Optional features
 router.get('/api/v1/trips/destination/:destination', api.getTripsByDest);
